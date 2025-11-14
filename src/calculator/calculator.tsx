f2621c947869c52c074e70afc34a8ab76dd0a472
Ruzsa Gergő <ruzsa.gergo@avander.hu>
2025-11-14 13:17:21 +0100

    refactor: reorganize fuel type selection component for improved structure

--- src/calculator/calculator.tsx
+++ src/calculator/calculator.tsx
@@ -62,37 +62,7 @@ export default function Calculator({
 
   return (
     <div style={{ paddingBottom: "10px" }}>
-      <Center>
-        <Stack align="center" w="190px">
-          <Field.Root w={"100%"} className="calculator-field">
-            <Field.Label>Select fuel type:</Field.Label>
-            <RadioGroup.Root
-              colorPalette={"teal"}
-              defaultValue={fuelTypes[0].value}
-            >
-              <HStack gap="6">
-                {fuelTypes.map((ft) => (
-                  <RadioGroup.Item
-                    key={ft.value}
-                    value={ft.value}
-                    colorPalette={"gray.500"}
-                    onChange={() => {
-                      setFuelType(ft.value);
-                    }}
-                  >
-                    <RadioGroup.ItemHiddenInput />
-                    <RadioGroup.ItemIndicator />
-                    <RadioGroup.ItemText>{ft.label}</RadioGroup.ItemText>
-                  </RadioGroup.Item>
-                ))}
-              </HStack>
-            </RadioGroup.Root>
-          </Field.Root>
-        </Stack>
-      </Center>
-
       <br />
-
       <Stack
         gap="4"
         align={"center"}
@@ -126,6 +96,34 @@ export default function Calculator({
             />
           </NumberInput.Root>
         </Field.Root>
+
+        <Stack align="center" w="190px">
+          <Field.Root w={"100%"} className="calculator-field">
+            <Field.Label>Select fuel type:</Field.Label>
+            <RadioGroup.Root
+              colorPalette={"teal"}
+              defaultValue={fuelTypes[0].value}
+            >
+              <HStack gap="6">
+                {fuelTypes.map((ft) => (
+                  <RadioGroup.Item
+                    key={ft.value}
+                    value={ft.value}
+                    colorPalette={"gray.500"}
+                    onChange={() => {
+                      setFuelType(ft.value);
+                    }}
+                  >
+                    <RadioGroup.ItemHiddenInput />
+                    <RadioGroup.ItemIndicator />
+                    <RadioGroup.ItemText>{ft.label}</RadioGroup.ItemText>
+                  </RadioGroup.Item>
+                ))}
+              </HStack>
+            </RadioGroup.Root>
+          </Field.Root>
+        </Stack>
+
         <Field.Root w={"200px"}>
           <Field.Label>Fuel price (HUF/L)</Field.Label>
 
